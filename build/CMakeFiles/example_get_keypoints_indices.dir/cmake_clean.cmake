ed73710e6d80422f44538ccf8c3e12317a8fec92
x86isnice <x86isnice@gmail.com>
2016-04-26 14:08:13 +0800

    changes log

---
+++ build/CMakeFiles/example_get_keypoints_indices.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_get_keypoints_indices.dir/example_get_keypoints_indices.cpp.o"
+  "example_get_keypoints_indices.pdb"
+  "example_get_keypoints_indices"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_get_keypoints_indices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
